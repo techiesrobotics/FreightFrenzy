37d7640da5406681a44d02d37c0a4c5c8305ee1c
karenbingfengli <karenbingfengli@gmail.com>
2021-11-14 16:32:08 -0500

    add vuforia and it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyTensorFlowObjectDetection.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -95,7 +96,7 @@ public class FreightFrenzyTensorFlowObjectDetection extends LinearOpMode {
      * localization engine.
      */
     private VuforiaLocalizer vuforia;
-
+    private String Level;
     /**
      * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
      * Detection engine.
@@ -114,15 +115,18 @@ public class FreightFrenzyTensorFlowObjectDetection extends LinearOpMode {
          * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
          **/
         if (tfod != null) {
-            tfod.activate();
 
-            // The TensorFlow software will scale the input images from the camera to a lower resolution.
-            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
-            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-            // should be set to the value of the images used to create the TensorFlow Object Detection model
-            // (typically 16/9).
-            tfod.setZoom(2.5, 16.0/9.0);
+                tfod.activate();
+
+                // The TensorFlow software will scale the input images from the camera to a lower resolution.
+                // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+                // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+                // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+                // should be set to the value of the images used to create the TensorFlow Object Detection model
+                // (typically 16/9).
+                tfod.setZoom(1.1, 16.0 / 5.0);
+
+
         }
 
         /** Wait for the game to begin */
@@ -147,6 +151,24 @@ public class FreightFrenzyTensorFlowObjectDetection extends LinearOpMode {
                                           recognition.getLeft(), recognition.getTop());
                         telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
                                 recognition.getRight(), recognition.getBottom());
+                        Double angle = Double.valueOf( recognition.estimateAngleToObject(AngleUnit.DEGREES));
+                        telemetry.addData(String.format("  Angle: "), angle);
+
+                        if (angle.doubleValue() <= -7){
+                            Level = "One";
+                            telemetry.addData(String.format("  Level: "), Level);
+                        }
+                        else if (angle.doubleValue() >= 7){
+                            Level = "Three";
+                            telemetry.addData(String.format("  Level: "), Level);
+
+                        }
+                        else {
+                            Level = "Two";
+                            telemetry.addData(String.format("  Level: "), Level);
+                        }
+
+
                         i++;
                       }
                       telemetry.update();
@@ -182,7 +204,7 @@ public class FreightFrenzyTensorFlowObjectDetection extends LinearOpMode {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
             "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.minResultConfidence = 0.7f;
         tfodParameters.isModelTensorFlow2 = true;
         tfodParameters.inputSize = 320;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
